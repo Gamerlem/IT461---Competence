99d510f96cc683c98ffe9b7d4f971d9c8dd8933b
Mel Jay Llanos <mel.jay.llanos@gmail.com>
2022-06-05 11:12:54 +0800

    header component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 
 
 const Home = () =>{
     return(
         <div>
-            <p>hello world</p>
+            <Header />
         </div>
     );
 }
